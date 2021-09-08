9ceae8560a460e56ef2f01d0eadf134768bd1fd1
wojkos9 <wojkos9@gmail.com>
2021-09-08 21:52:26 +0200

    Convert CRLF to LF so that newline encoding is consistent

--- core/utils.py
+++ core/utils.py
@@ -4,5 +4,6 @@ import string
 ALPHANUMERIC_CHARS = string.ascii_lowercase + string.digits
 STRING_LENGTH = 6
 
+
 def generate_random_string(chars=ALPHANUMERIC_CHARS, length=STRING_LENGTH):
     return "".join(str(random.choice(chars)) for x in range(length))
