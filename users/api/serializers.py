72f1ca271915f17797177961b924fee227deec86
Lewand118 <49614673+Lewand118@users.noreply.github.com>
2021-09-12 18:55:29 +0200

    user-update

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -1,9 +1,18 @@
+from django.db.models.query import InstanceCheckMeta
 from rest_framework import serializers
 from users.models import CustomUser
+from voting.models import Group
+
 
 class UserDisplaySerializer(serializers.ModelSerializer):
 
+    groups = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = CustomUser
-        fields="__all__"
+        fields = "__all__"
 
+    def get_groups(self, instance):
+        user_groups = Group.objects.filter(members=instance.pk).values()
+
+        return user_groups
