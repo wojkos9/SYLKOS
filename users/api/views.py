09b5d1ce29f7c6308b3bef6510811c018160a66e
Wojciech Koszela <wojkos9@gmail.com>
2021-09-13 22:49:46 +0200

    Create PersonalKey model. Make user registration require key.

--- users/api/views.py
+++ users/api/views.py
@@ -1,9 +1,32 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from users.api.serializers import UserDisplaySerializer
+from rest_framework.generics import CreateAPIView
+from rest_framework.parsers import JSONParser
+from rest_framework.request import Request
+from django import http
+from users.api.serializers import UserDisplaySerializer, CustomUserSerializer
+
+from django.views.decorators.csrf import csrf_exempt
 
 class CurrentUserAPIView(APIView):
 
     def get(self, request):
         serializer = UserDisplaySerializer(request.user)
         return Response(serializer.data)
+
+
+class CreateCustomUserView(CreateAPIView):
+    serializer_class = CustomUserSerializer
+    parser_classes = [JSONParser]
+    permission_classes = []
+
+    @csrf_exempt  # TODO: Delete
+    def post(self, q: Request):
+        serializer: CustomUserSerializer = self.serializer_class(data=q.data)
+        try:
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return http.HttpResponse("OK")
+            return http.HttpResponseBadRequest()
+        except Exception as e:
+            return http.HttpResponseBadRequest(e)
--- users/models.py
+++ users/models.py
@@ -3,6 +3,12 @@ from django.db import models
 from voting.models import Group, Voting
 
 
+class PersonalKey(models.Model):
+    value = models.IntegerField(primary_key=True)  # TODO: Change to more reasonable type
+    used = models.IntegerField(default=0)
+
+
 class CustomUser(AbstractUser):
+    key = models.ForeignKey(PersonalKey, on_delete=models.DO_NOTHING, null=True)
     groups = models.ManyToManyField(Group, related_name="user_groups", blank=True)
     voting_history = models.ManyToManyField(Voting, related_name="user_voting_history", blank=True)
