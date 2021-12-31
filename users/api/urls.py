daff31b2e4f055486f8305219c906cca1b46d861
wojkos9 <wojkos9@gmail.com>
2021-12-31 12:10:38 +0100

    Project authorship WIP + move P/G to separate urls

--- users/api/urls.py
+++ users/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from users.api.views import CurrentUserAPIView, CreateCustomUserView, user_grant_rights
+from users.api.views import CurrentUserAPIView, CreateCustomUserView, UserGroupsView, UserProjectsView, user_grant_rights
 
 urlpatterns = [
     path("user/", CurrentUserAPIView.as_view(), name="current-user"),
+    path("user/projects", UserProjectsView.as_view(), name="current-user-projects"),
+    path("user/groups", UserGroupsView.as_view(), name="current-user-groups"),
     path("user/<int:pk>/grant", user_grant_rights),
     path("create_user/", CreateCustomUserView.as_view())
 ]
