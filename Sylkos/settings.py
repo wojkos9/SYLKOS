63eea6f96eada265ad0df775468fc3c23fa73a74
krzk426 <krzk426@gmail.com>
2022-01-06 19:31:22 +0100

    logoin register screen

--- Sylkos/settings.py
+++ Sylkos/settings.py
@@ -125,7 +125,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pl-PL'
 
 TIME_ZONE = 'UTC'
 
